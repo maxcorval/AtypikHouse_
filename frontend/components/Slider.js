a0d67ea8a989db1eef38a03208aef80bfe4345dc
maxcorval <vidjinelodji@yahoo.com>
2022-10-11 11:42:30 +0200

    a

---
+++ frontend/components/Slider.js
@@ -0,0 +1,28 @@
+import { Splide, SplideSlide } from '@splidejs/react-splide';
+//import '../node_modules/@splidejs/splide/dist/css/themes/splides-sea-green';
+
+export default function Slider() {
+  return (
+    
+    <Splide options={ { rewind: true } } aria-label="React Splide Example" >
+      <SplideSlide>
+        <img src="images/1/chambre1.jpg" alt="Chambre"/>
+      </SplideSlide>
+      <SplideSlide>
+        <img src="images/1/salon1.jpg" alt="Salon"/>
+      </SplideSlide>
+      <SplideSlide>
+        <img src="images/1/cuisine1.jpg" alt="Cuisine"/>
+      </SplideSlide>
+      <SplideSlide>
+        <img src="images/1/jardin1.jpg" alt="Jardin"/>
+      </SplideSlide>
+      <SplideSlide>
+        <img src="images/1/piscine1.jpg" alt="Piscine"/>
+      </SplideSlide>
+      <SplideSlide>
+        <img src="images/1/douche1.jpg" alt="Douche"/>
+      </SplideSlide>
+    </Splide>
+  );
+}
--- frontend/components/screens/HomeScreen.js
+++ frontend/components/screens/HomeScreen.js
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import { View, Text } from 'react-native';
+import Slider from '../Slider';
 
 export default function HomeScreen({ navigation }) {
     return (
@@ -7,6 +8,7 @@ export default function HomeScreen({ navigation }) {
             <Text
                 onPress={() => alert('This is the "Home" screen.')}
                 style={{ fontSize: 26, fontWeight: 'bold' }}>Home Screen</Text>
+                <Slider/>
         </View>
     );
 }
